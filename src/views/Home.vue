ae8ffa7dcd3bc129f86d7315f608a1102e78ed75
lucastimho <lucas.t.ho.lh@gmail.com>
2021-12-06 13:12:07 -0600

    adds live games

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Dota logo" src="../assets/dota_stock_image.jpeg" />
     <h1>{{ message }}</h1>
+    <div v-for="game in games" :key="game.match_id.parseInt()">{{ game.players }}</div>
+    <!-- <div class="row">
+      <div class="column"></div>
+      <div class="column"></div>
+    </div> -->
   </div>
 </template>
 
+<style>
+img {
+  width: 700px;
+}
+.column {
+  float: left;
+  width: 50%;
+}
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
@@ -14,7 +35,15 @@ export default {
     };
   },
   created: function () {
-    // this.livesGame();
+    this.liveGames();
+  },
+  methods: {
+    liveGames: function () {
+      axios.get("https://api.opendota.com/api/live").then((response) => {
+        this.games = response.data;
+        console.log("Recent Games", this.games);
+      });
+    },
   },
 };
 </script>
