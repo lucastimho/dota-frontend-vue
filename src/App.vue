21bf300f526ba0f2e847a8c8deabab94788fd407
lucastimho <lucas.t.ho.lh@gmail.com>
2021-12-06 12:37:31 -0600

    adds navbar and login v-if

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link>
       |
       <router-link to="/players">Pro Players</router-link>
@@ -12,7 +12,63 @@
       <router-link to="/login">Login</router-link>
       |
       <router-link to="/logout">Logout</router-link>
-    </div>
+    </div> -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Home</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/players">All Pro Players</a>
+            </li>
+            <li class="nav-item" v-if="!isLoggedIn()">
+              <a class="nav-link" href="/login">Login</a>
+            </li>
+            <li class="nav-item" v-if="isLoggedIn()">
+              <a class="nav-link" href="/logout">Logout</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" v-if="isLoggedIn()" href="/following">Follow List</a></li>
+                <li><a class="dropdown-item" href="/about">About</a></li>
+                <div v-if="!isLoggedIn()">
+                  <li><hr class="dropdown-divider" /></li>
+                  <li><a class="dropdown-item" href="/signup">Sign Up</a></li>
+                </div>
+              </ul>
+            </li>
+            <li class="nav-item" v-if="!isLoggedIn()">
+              <a class="nav-link disabled">Welcome Guest</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
@@ -39,6 +95,23 @@
   color: #42b983;
 }
 body {
-  background-image: url("./assets/double-bubble-dark.png");
+  background-image: url("./assets/double-bubble.png");
 }
 </style>
+
+<script>
+export default {
+  methods: {
+    isLoggedIn: function () {
+      if (localStorage.getItem("jwt")) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    getUserId: function () {
+      return localStorage.getItem("user_id");
+    },
+  },
+};
+</script>
