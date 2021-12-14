9021d424e62baefa8c227aa2fd56e347168d0865
lucastimho <lucas.t.ho.lh@gmail.com>
2021-12-14 14:23:12 -0600

    adds recent games data to displayon home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,25 @@
   <div class="home">
     <img alt="Dota logo" src="../assets/dota_stock_image.jpeg" />
     <h1>{{ message }}</h1>
-    <div v-for="game in games" :key="game.match_id">{{ game.players }}</div>
-    <!-- <div class="row">
-      <div class="column"></div>
-      <div class="column"></div>
-    </div> -->
+    <div v-if="!isLoggedIn()">
+      <div v-for="game in liveGamesNow" :key="game.match_id">{{ game.players }}</div>
+    </div>
+    <div v-if="isLoggedIn()">
+      <div v-for="game in recentGamesPlayed" :key="game.match_id">
+        <h2>Radiant Victory: {{ game.radiant_win }}</h2>
+        <p>Game Duration: {{ game.duration }}</p>
+        <p>Kills: {{ game.kills }}</p>
+        <p>Deaths: {{ game.deaths }}</p>
+        <p>Assists: {{ game.assists }}</p>
+        <p>XPM: {{ game.xp_per_min }}</p>
+        <p>GPM: {{ game.gold_per_min }}</p>
+        <p>Hero Damage: {{ game.hero_damage }}</p>
+        <p>Tower Damage: {{ game.tower_damage }}</p>
+        <p>Healing: {{ game.hero_healing }}</p>
+        <p>Last Hits: {{ game.last_hits }}</p>
+        <p>Role: {{ game.lane_role }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,18 +45,33 @@ export default {
   data: function () {
     return {
       message: "Welcome to the Dota 2 Database",
-      games: {},
+      liveGamesNow: {},
+      recentGamesPlayed: {},
     };
   },
   created: function () {
     this.liveGames();
+    this.recentGames();
   },
   methods: {
     liveGames: function () {
       axios.get("https://api.opendota.com/api/live").then((response) => {
-        this.games = response.data;
-        console.log("Recent Games", this.games);
+        this.liveGamesNow = response.data;
+        console.log("Live Games", this.liveGamesNow);
       });
+    },
+    isLoggedIn: function () {
+      if (localStorage.getItem("jwt")) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    recentGames: function () {
+      axios.get("/recent").then((response) => {
+        this.recentGamesPlayed = response.data;
+      });
+      console.log("Recent Games", this.recentGamesPlayed);
     },
   },
 };
