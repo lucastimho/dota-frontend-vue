943c930f680d472f44bf170271ced79255f8fc74
lucastimho <lucas.t.ho.lh@gmail.com>
2021-12-16 13:33:42 -0600

    designs about page

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,4 @@
 <template>
-  <!-- <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">Home</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/players">All Pro Players</a>
-            </li>
-            <li class="nav-item" v-if="!isLoggedIn()">
-              <a class="nav-link" href="/login">Login</a>
-            </li>
-            <li class="nav-item" v-if="isLoggedIn()">
-              <a class="nav-link" href="/logout">Logout</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                More
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" v-if="isLoggedIn()" href="/following">Follow List</a></li>
-                <li><a class="dropdown-item" v-if="isLoggedIn()" href="/profile">Profile Settings</a></li>
-                <li><a class="dropdown-item" href="/about">About</a></li>
-                <div v-if="!isLoggedIn()">
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="/signup">Sign Up</a></li>
-                </div>
-              </ul>
-            </li>
-            <li class="nav-item" v-if="!isLoggedIn()">
-              <a class="nav-link disabled">Welcome Guest</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </div> -->
   <div>
     <!-- ======= Header ======= -->
     <header id="header" class="d-flex align-items-center">
@@ -64,11 +9,13 @@
           <ul>
             <li><a class="nav-link scrollto active" href="/">Home</a></li>
             <li><a class="nav-link scrollto" href="/about">About</a></li>
-            <li><a class="nav-link scrollto" href="#services">Services</a></li>
-            <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-            <li><a class="nav-link scrollto" href="#testimonials">Testimonials</a></li>
-            <li><a class="nav-link scrollto" href="#team">Team</a></li>
-            <li class="dropdown">
+            <li><a class="nav-link scrollto" href="/games">Games</a></li>
+            <li><a class="nav-link scrollto" href="/players">Pro Players</a></li>
+            <li v-if="!isLoggedIn()"><a class="nav-link scrollto" href="/login">Login</a></li>
+            <li v-if="isLoggedIn()"><a class="nav-link scrollto" href="/following">Following</a></li>
+            <li v-if="isLoggedIn()"><a class="nav-link scrollto" href="/logout">Logout</a></li>
+            <li v-if="isLoggedIn()"><a href="/profile">Profile</a></li>
+            <!-- <li class="dropdown">
               <a href="#">
                 <span>Drop Down</span>
                 <i class="bi bi-chevron-down"></i>
@@ -92,8 +39,9 @@
                 <li><a href="#">Drop Down 3</a></li>
                 <li><a href="#">Drop Down 4</a></li>
               </ul>
-            </li>
+            </li> -->
             <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+            <li v-if="!isLoggedIn()"><a class="nav-link scrollto" href="#">Welcome Guest</a></li>
           </ul>
           <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
@@ -141,9 +89,6 @@ export default {
       } else {
         return false;
       }
-    },
-    getUserId: function () {
-      return localStorage.getItem("user_id");
     },
   },
 };
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,40 @@
 <template>
-  <div class="about">
-    <h1>About This Database</h1>
-    <h3>
-      Well I made this database for a project for Actualize. There was a lot of issues making this come together but I'm
-      glad I stuck through with it. I used an API to get me all the data that you find on this application. I really
-      enjoy playing Dota 2 and that's why I pursued making this database. Would I use this personally. Honestly no,
-      because I'm not that interested in following players. That does not mean I'm not proud of my work though, I
-      throughly enjoyed working on every piece of code despite all the headaches (ugh). Anyways, thank you for checking
-      out this application and for reading this page. Now go back and look through this application some more!
-    </h3>
+  <!-- ======= About Section ======= -->
+  <div>
+    <section id="about">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="col-md-12">
+            <h3 class="section-title">About This Database</h3>
+            <div class="section-title-divider"></div>
+            <!-- <p class="section-description">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+            aperiam
+          </p> -->
+          </div>
+        </div>
+      </div>
+      <div class="container about-container" data-aos="fade-up" data-aos-delay="200">
+        <div class="row">
+          <div class="col-lg-6 about-img">
+            <img src="../assets/dota_stock_image.jpeg" alt="" />
+          </div>
+
+          <div class="col-md-6 about-content">
+            <h2 class="about-title">The story behind this database</h2>
+            <p class="about-text">
+              Well I made this database for a project for Actualize. There was a lot of issues making this come together
+              but I'm glad I stuck through with it. I used an API to get me all the data that you find on this
+              application. I really enjoy playing Dota 2 and that's why I pursued making this database. Would I use this
+              personally. Honestly no, because I'm not that interested in following players. That does not mean I'm not
+              proud of my work though, I throughly enjoyed working on every piece of code despite all the headaches
+              (ugh). Anyways, thank you for checking out this application and for reading this page. Now go back and
+              look through this application some more!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End About Section -->
   </div>
 </template>
