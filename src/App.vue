8f960b8d3ab954d12df4754f3cf018600b403b93
lucastimho <lucas.t.ho.lh@gmail.com>
2021-12-16 12:40:36 -0600

    designs home page

--- src/App.vue
+++ src/App.vue
@@ -54,24 +54,7 @@
       </div>
     </nav>
   </div> -->
-  <!-- ======= Hero Section ======= -->
   <div>
-    <section id="hero">
-      <div class="hero-container">
-        <div data-aos="fade-in">
-          <h1>Welcome to the Dota 2 Database</h1>
-          <h2>
-            We create
-            <span class="typed" data-typed-items="beautiful graphics, functional websites, working mobile apps"></span>
-          </h2>
-          <div class="actions">
-            <a href="#about" class="btn-get-started">Get Started</a>
-            <a href="#services" class="btn-services">Our Services</a>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- End Hero -->
     <!-- ======= Header ======= -->
     <header id="header" class="d-flex align-items-center">
       <div class="container d-flex align-items-center justify-content-between">
@@ -118,6 +101,32 @@
       </div>
     </header>
     <!-- End Header -->
+    <router-view />
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="copyright">
+              &copy; Copyright
+              <strong>Imperial Theme</strong>
+              . All Rights Reserved
+            </div>
+            <div class="credits">
+              <!--
+            All the links in the footer should remain intact.
+            You can delete the links only if you purchased the pro version.
+            Licensing information: https://bootstrapmade.com/license/
+            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Imperial
+          -->
+              Designed by
+              <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- End Footer -->
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,32 @@
 <template>
-  <div class="home">
-    <div v-if="!isLoggedIn()">
-      <div v-for="game in liveGamesNow" :key="game.match_id">
-        -----------------------------------------------
-
-        <p>Average MMR: {{ game.average_mmr }}</p>
-        <p>Game Time: {{ game.game_time }}</p>
+  <!-- ======= Hero Section ======= -->
+  <div>
+    <section id="hero">
+      <div class="hero-container">
+        <div data-aos="fade-in">
+          <h1>Welcome to the Dota 2 Database</h1>
+          <h2>
+            Look up
+            <span class="typed" data-typed-items="live games, pro players, recent games, your favorite players"></span>
+          </h2>
+          <div class="actions" v-if="!isLoggedIn()">
+            <a href="#about" class="btn-get-started">Sign Up</a>
+            <a href="#services" class="btn-services">Live Games</a>
+          </div>
+          <div class="actions" v-if="isLoggedIn()">
+            <a href="#about" class="btn-get-started">Following</a>
+            <a href="#services" class="btn-services">Recent Games</a>
+          </div>
+        </div>
       </div>
-    </div>
-    <div v-if="isLoggedIn()">
-      <div v-for="game in recentGamesPlayed" :key="game.match_id">
-        <h2>Radiant Victory: {{ game.radiant_win }}</h2>
-        <p>Game Duration: {{ game.duration }}</p>
-        <p>Kills: {{ game.kills }}</p>
-        <p>Deaths: {{ game.deaths }}</p>
-        <p>Assists: {{ game.assists }}</p>
-        <p>XPM: {{ game.xp_per_min }}</p>
-        <p>GPM: {{ game.gold_per_min }}</p>
-        <p>Hero Damage: {{ game.hero_damage }}</p>
-        <p>Tower Damage: {{ game.tower_damage }}</p>
-        <p>Healing: {{ game.hero_healing }}</p>
-        <p>Last Hits: {{ game.last_hits }}</p>
-        <p>Role: {{ game.lane_role }}</p>
-      </div>
-    </div>
+    </section>
+    <!-- End Hero -->
   </div>
 </template>
 
-<style></style>
-
 <script>
-import axios from "axios";
 export default {
-  data: function () {
-    return {
-      liveGamesNow: {},
-      recentGamesPlayed: {},
-    };
-  },
-  created: function () {
-    this.liveGames();
-    this.recentGames();
-  },
   methods: {
-    liveGames: function () {
-      axios.get("https://api.opendota.com/api/live").then((response) => {
-        this.liveGamesNow = response.data;
-        console.log("Live Games", this.liveGamesNow);
-      });
-    },
     isLoggedIn: function () {
       if (localStorage.getItem("jwt")) {
         return true;
@@ -56,11 +34,8 @@ export default {
         return false;
       }
     },
-    recentGames: function () {
-      axios.get("/recent").then((response) => {
-        this.recentGamesPlayed = response.data;
-      });
-      console.log("Recent Games", this.recentGamesPlayed);
+    getUserId: function () {
+      return localStorage.getItem("user_id");
     },
   },
 };
