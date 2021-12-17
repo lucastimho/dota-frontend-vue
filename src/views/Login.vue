a017bd2c029b771a45591c22d28202fc067cdabc
lucastimho <lucas.t.ho.lh@gmail.com>
2021-12-17 13:54:38 -0600

    designs profile settings page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <form v-on:submit.prevent="submit()">
+    <!-- <form v-on:submit.prevent="submit()">
       <h1>Login</h1>
       <ul>
         <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
@@ -14,7 +14,73 @@
         <input type="password" v-model="newSessionParams.password" />
       </div>
       <input type="submit" value="Submit" />
-    </form>
+    </form> -->
+    <!-- ======= Contact Section ======= -->
+    <section id="contact">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="col-md-12">
+            <h3 class="section-title">Login</h3>
+            <div class="section-title-divider"></div>
+            <p class="section-description">
+              Welcome to the Dota 2 Database. It's great to see you again. Please log in so you can use all the features
+              of the database.
+            </p>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-3 col-md-4">
+            <div class="info">
+              <div>
+                <i class="bi bi-github"></i>
+                <p>github.com/lucastimho</p>
+              </div>
+
+              <div>
+                <i class="bi bi-linkedin"></i>
+                <p>
+                  linkedin.com/in/
+                  <br />
+                  lucas-t-ho
+                </p>
+              </div>
+
+              <div>
+                <i class="bi bi-envelope-plus-fill"></i>
+                <p>lucas.t.ho.lh@gmail.com</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-5 col-md-8">
+            <div class="form">
+              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <div class="form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required />
+                </div>
+                <div class="form-group mt-3">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required />
+                </div>
+                <div class="form-group mt-3">
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required />
+                </div>
+                <div class="form-group mt-3">
+                  <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                </div>
+                <div class="my-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Send Message</button></div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Contact Section -->
   </div>
 </template>
 
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,14 +1,57 @@
 <template>
   <div>
-    <h1>Profile page</h1>
-    <form v-on:submit.prevent="updateUser(user)">
-      <ul>
-        <li v-for="error in errors" :key="error">{{ error }}</li>
-      </ul>
-      <h3>Account ID:</h3>
-      <input v-model="user.account_id" />
-      <input type="submit" value="Submit" />
-    </form>
+    <!-- ======= Contact Section ======= -->
+    <section id="contact">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="col-md-12">
+            <h3 class="section-title">Profile Page</h3>
+            <div class="section-title-divider"></div>
+            <p class="section-description">Need to make changes to your account ID? You come to the right place.</p>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-3 col-md-4">
+            <div class="info">
+              <div>
+                <i class="bi bi-github"></i>
+                <p>github.com/lucastimho</p>
+              </div>
+
+              <div>
+                <i class="bi bi-linkedin"></i>
+                <p>
+                  linkedin.com/in/
+                  <br />
+                  lucas-t-ho
+                </p>
+              </div>
+
+              <div>
+                <i class="bi bi-envelope-plus-fill"></i>
+                <p>lucas.t.ho.lh@gmail.com</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-5 col-md-8">
+            <div class="form">
+              <ul>
+                <li v-for="error in errors" :key="error">{{ error }}</li>
+              </ul>
+              <form v-on:submit.prevent="updateUser(user)">
+                <div class="form-group">
+                  <input type="text" class="form-control" :placeholder="user.account_id" required />
+                </div>
+                <button class="btn btn-primary" type="submit" value="Submit">Update ID</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Contact Section -->
   </div>
 </template>
 
