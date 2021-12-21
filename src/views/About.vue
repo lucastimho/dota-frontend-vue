959df16a32a1121a6913c1dba6d5e1d8b17f0921
lucastimho <lucas.t.ho.lh@gmail.com>
2021-12-21 11:59:43 -0600

    fixes punctuation mark

--- src/views/About.vue
+++ src/views/About.vue
@@ -21,7 +21,7 @@
               Well I made this database for a project for Actualize. There was a lot of issues making this come together
               but I'm glad I stuck through with it. I used an API to get me all the data that you find on this
               application. I really enjoy playing Dota 2 and that's why I pursued making this database. Would I use this
-              personally. Honestly no, because I'm not that interested in following players. That does not mean I'm not
+              personally? Honestly no, because I'm not that interested in following players. That does not mean I'm not
               proud of my work though, I throughly enjoyed working on every piece of code despite all the headaches
               (ugh). Anyways, thank you for checking out this application and for reading this page. Now go back and
               look through this application some more!
